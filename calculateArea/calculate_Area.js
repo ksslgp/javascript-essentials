191def5c8770e8bd524d7857151848f48dca8acc
ksslgp <ksslgruppe@gmail.com>
2025-09-06 12:37:02 -0400

    add grocery-total function practice

--- calculateArea/calculate_Area.js
+++ calculateArea/calculate_Area.js
@@ -8,4 +8,12 @@ function calculateArea() {
    let area = length * width;
    document.getElementById('result').innerText = `The area of the rectangle is: ${area}`;
    } 
-   
+  
+function calculateTotal() {
+    amount1 = parseFloat(document.getElementById('amount-1').value);
+    amount2 = parseFloat(document.getElementById('amount-2').value);
+    amount3 = parseFloat(document.getElementById('amount-3').value);
+
+    let ttlAmount = amount1 + amount2 + amount3;
+   document.getElementById('totalAmount').innerText = `The total amount of grocery is: ${ttlAmount}`;
+   } 
