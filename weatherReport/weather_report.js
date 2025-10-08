0040ca2a60fa412f89bb4e059bed03897deed5a2
ksslgp <ksslgruppe@gmail.com>
2025-10-08 13:06:21 -0400

    Add check Weather by Geocode

--- weatherReport/weather_report.js
+++ weatherReport/weather_report.js
@@ -2,7 +2,7 @@ function showweatherDetails(event) {
     event.preventDefault();
 
     const city = document.getElementById('city').value;
-    const apiKey = '2ea139fdc3968cb1710188a9fe'; // Replace 'YOUR_API_KEY' with your actual API key
+    const apiKey = ''; // Replace 'YOUR_API_KEY' with your actual API key
     const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
     fetch(apiUrl)
@@ -20,4 +20,29 @@ function showweatherDetails(event) {
         })
 }
 
+function showweatherByGeoCode(event) {
+    event.preventDefault();
+
+    const lat = document.getElementById('latitudeInput').value;
+    const lon = document.getElementById('longitudeInput').value;
+    const apiKey = ''; // Replace 'YOUR_API_KEY' with your actual API key
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}`;
+
+    fetch(apiUrl)
+        .then(response => response.json())
+        .then(data => {
+            const weatherInfo = document.getElementById('weatherInfo');
+            weatherInfo.innerHTML = `<h2>Weather in ${data.name}</h2>
+                                  <p>Temperature: ${data.main.temp} &#8451;</p>
+                                  <p>Weather: ${data.weather[0].description}</p>`;                                  
+        })
+        .catch(error => {
+            console.error('Error fetching weather:', error);
+            const weatherInfo = document.getElementById('weatherInfo');
+            weatherInfo.innerHTML = `<p>Failed to fetch weather. Please try again.</p>`;
+        })
+}
+
 document.getElementById('weatherForm').addEventListener('submit', showweatherDetails);
+
+document.getElementById('weatherFormByGeoCode').addEventListener('submit', showweatherByGeoCode);
